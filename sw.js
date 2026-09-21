bf406bd88d7cbe51432f4b0347c2dd8d68878b21
AntoPortelance <anto.portelance@gmail.com>
2026-09-22 03:13:58 +0900

    Move language and sound options into a Settings menu

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Change le numéro de version à chaque mise à jour du jeu
-const CACHE = "natmal-v2";
+const CACHE = "natmal-v3";
 const CORE = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
