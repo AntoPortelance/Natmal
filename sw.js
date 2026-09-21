0e217e11e36f02e3bb360d01af8391d6b4c27698
AntoPortelance <anto.portelance@gmail.com>
2026-09-22 04:21:52 +0900

    Exam Random draws
    Make exams random draws from words not yet validated

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Change le numéro de version à chaque mise à jour du jeu
-const CACHE = "natmal-v7";
+const CACHE = "natmal-v8";
 const CORE = ["./", "./index.html", "./manifest.webmanifest", "./icon-180.png", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
